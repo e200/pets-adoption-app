b6122ae7fc798deda3c0f05a0aedec673c0df29d
Eleandro Duzentos <eleandro@inbox.ru>
2020-07-19 22:04:29 +0100

    Added google font

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:pets_adoption_app/theme/icon.dart';
 
 class AppTheme {
   static ThemeData getTheme(BuildContext context) {
     return ThemeData(
+      primaryColor: Colors.green.shade900,
       iconTheme: getIconTheme(context),
+      textTheme: GoogleFonts.openSansTextTheme(),
     );
   }
 }
